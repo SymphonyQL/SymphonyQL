7c5e2f3ea3c779ec26b8853f9cd69e9985894631
jxnu-liguobin <dreamylost@outlook.com>
2024-02-02 19:00:45 +0800

    Fix

--- examples/src/main/scala/example/schema/UserAPI.scala
+++ examples/src/main/scala/example/schema/UserAPI.scala
@@ -3,8 +3,6 @@ package example.schema
 import org.apache.pekko.NotUsed
 import org.apache.pekko.stream.scaladsl.*
 
-final case class UserUpdateInput(username: String, sex: Int)
-
 final case class UserQueryInput(id: String)
 
 final case class UserOutput(id: String, username: String)
