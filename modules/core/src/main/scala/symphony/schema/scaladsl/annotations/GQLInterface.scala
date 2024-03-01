7861f1e7605492728d1432acbfb60b8f808812a3
jxnu-liguobin <dreamylost@outlook.com>
2024-03-01 22:45:18 +0800

    fix

--- modules/core/src/main/scala/symphony/schema/scaladsl/annotations/GQLInterface.scala
+++ modules/core/src/main/scala/symphony/schema/scaladsl/annotations/GQLInterface.scala
@@ -3,8 +3,8 @@ package symphony.schema.scaladsl.annotations
 import scala.annotation.StaticAnnotation
 
 /**
- * Annotation to make a sealed trait an interface instead of a union type or an enum
+ * Annotation to make a sealed trait an interface instead of a union type or an enum.
  *
- * @param excludeFields Optionally provide a list of field names that should be excluded from the interface
+ * @param excludedFields Optionally provide a list of field names that should be excluded from the interface.
  */
 case class GQLInterface(excludedFields: String*) extends StaticAnnotation
